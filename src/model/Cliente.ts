be0d0b741b8387498149d88f1efca2f650cbebb1
Caroline060 <tatagumbio25@gmail.com>
2024-10-01 15:03:43 -0300

    criação das classes Cliente e PedidoVenda

---
+++ src/model/Cliente.ts
@@ -0,0 +1,102 @@
+/* Classe que representa cliente */
+export class Cliente{
+
+    /* Identificador do cliente */
+    private idCliente: number = 0;
+    /* nome do cliente */
+    private nome: string;
+    /* cpf do cliente */
+    private cpf: string;
+    /* telefone do cliente */
+    private telefone: string;
+
+    /**
+     * Construtor da classe Cliente
+     * 
+     * @param nome Nome do cliente
+     * @param cpf Cpf do cliente
+     * @param telefone Telefone do cliente
+     */
+    constructor (
+        idCliente: number,
+        nome: string,
+        cpf: string,
+        telefone: string
+    ){
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    /* Métodos get e set */
+    /**
+     * Recupera o identificador do cliente
+     * @returns o identificador do cliente
+     */
+    public getIdCliente(): number {
+        return this.idCliente;
+    }
+    /**
+     * Atribui um valor ao identificador do cliente
+     * @param idCliente novo identificado do cliente
+     */
+    public setIdCliente(idCliente: number): void{
+        this.idCliente = idCliente;
+    }
+
+    /**
+     * Retorna o nome do cliente.
+     *
+     * @returns {string} o nome do cliente.
+     */
+    public getNome(): string {
+        return this.nome; 
+    }
+
+    /**
+     * Define o nome do cliente.
+     * 
+     * @param nome - o nome do cliente a ser definido.
+     */
+    public setNome(nome: string): void{
+        this.nome = nome;
+    }
+
+    /**
+     * Retorna o cpf do cliente.
+     *
+     * @returns {string} o cpf do cliente.
+     */ 
+    public geCpf(): string {
+        return this.cpf;
+    }
+
+    /**
+     * Define o cpf do cliente.
+     *
+     * @param cpf - o número do cpf do cliente.
+     */
+    public setCpf(cpf: string): void{
+        this.cpf = cpf;
+    }
+
+    /**
+     * Retorna o telefone do carro.
+     *
+     * @returns O telefone do cliente.
+     */ 
+    public getTelefone(): string {
+        return this.telefone;
+    }
+    
+    /**
+     * Define telefone do cliente.
+     * 
+     * @param telefone - o numero do telefone do cliente.
+     */
+    public setTelefone(telefone: string): void{
+        this.telefone = telefone;
+    }
+}
+
---
+++ src/model/PedidoVenda.ts
@@ -0,0 +1,86 @@
+/* Classe que representa PedidoVenda */
+export class PedidoVenda{
+
+    /* Identificador do Pedido */
+    private idPedido: number = 0;
+    /* identificador do carro */
+    private idCarro: number = 0;
+    /* identificador do cliente */
+    private idCliente: number = 0;
+    /* data do pedido de venda*/
+    private dataPedido: Date;
+    /* valor do pedido de venda */
+    private valorPedido: number = 0;
+
+    /**
+     * Construtor da classe Pedido
+     * 
+     * @param idCarro Carro a ser vendido
+     * @param idCliente Cliente a fazer a compra
+     * @param dataPedido data do pedido de venda
+     * @param valorPedido valor do pedido
+     */
+    constructor (
+        idCarro: number,
+        idCliente: number,
+        dataPedido: Date,
+        valorPedido: number
+    ){
+        this.idCarro = idCarro;
+        this.idCliente = idCliente;
+        this.dataPedido = dataPedido;
+        this.valorPedido = valorPedido;
+    }
+
+    /* Métodos get e set */
+    /**
+     * Recupera o identificador do pedido
+     * @returns o identificador do pedido
+     */
+    public getIdPedido(): number {
+        return this.idPedido;
+    }
+    /**
+     * Atribui um valor ao identificador do pedido
+     * @param idPedido novo identificado do pedido
+     */
+    public setIdPedido(idPedido: number): void{
+        this.idPedido = idPedido;
+    }
+
+    /**
+     * Retorna a data do pedido.
+     *
+     * @returns {Date} a data do pedido.
+     */
+    public getDataPedido(): Date {
+        return this.dataPedido; 
+    }
+
+    /**
+     * Define a data do pedido.
+     * 
+     * @param dataPedido - a data do pedido a ser definida.
+     */
+    public setDataPedido(dataPedido: Date): void{
+        this.dataPedido = dataPedido;
+    }
+
+    /**
+     * Retorna o valor do pedido.
+     *
+     * @returns {number} o valor do pedido.
+     */ 
+    public geValorPedido(): number {
+        return this.valorPedido;
+    }
+
+    /**
+     * Define o valor do pedido.
+     *
+     * @param valorPedido - o valor do pedido feito.
+     */
+    public setValorPedido(valorPedido: number): void{
+        this.valorPedido = valorPedido;
+    }
+}
